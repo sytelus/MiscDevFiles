f940cc93b93df23dd772bcfde35bbfb68074123b
Shital Shah <sytelus@gmail.com>
2016-09-14 16:53:50 -0700

    Update copynewfiles.bat

--- source/backup/copynewfiles.bat
+++ source/backup/copynewfiles.bat
@@ -1,5 +1,4 @@
 time /t
 REM Copy all folders with timestamps
-robocopy %1 %2 /E /SL /DCOPY:DAT /R:0 /NP /TEE /LOG:c:\temp\copynewfiles.log /XF c:\temp\copynewfiles.log
+robocopy %1 %2 /E /SL /XJ /DCOPY:DAT /R:0 /NP /TEE /LOG:c:\temp\copynewfiles.log /XF c:\temp\copynewfiles.log
 time /t
-pause
